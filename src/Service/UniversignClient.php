b50c728bd68c5a3293554efb6ae723cce1b7f89b
jgondellier <jgondellier@yahoo.fr>
2024-03-28 11:37:03 +0100

    Update UniversignClient.php

--- src/Service/UniversignClient.php
+++ src/Service/UniversignClient.php
@@ -6,5 +6,15 @@ use Universign\Rest\ClientComponent\Model\certificatesMatch;
 
 class UniversignClient
 {
+    protected CertificatesMatchClient $certificatesMatchClient;
 
+    public function __construct(string $uri, LoggerInterface $logger = null)
+    {
+        $this->uri = $uri;
+        $this->certificatesMatchClient = new CertificatesMatchClient($uri, $logger);
+    }
+
+    public function certificatesMatch(certificatesMatch $certificatesMatch){
+        return $this->certificatesMatchClient->certificatesMatch($certificatesMatch);
+    }
 }
